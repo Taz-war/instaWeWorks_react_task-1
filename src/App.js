bfe3292bbfea96c55a274a8f66208a9ceb6d5c88
Taz-war <89224568+Taz-war@users.noreply.github.com>
2023-03-27 03:24:38 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import Feed from "./components/Feed";
 
 import RightBar from "./components/RightBar";
 import Box from "@mui/material/Box";
-import { Stack } from "@mui/material";
+import { createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode,setMode] = useState("light")
+  const darkTheme = createTheme({
+    palette:{
+      mode:mode
+    }
+  })
   return (
-    <Box>
+    <ThemeProvider theme={darkTheme}>
+    <Box bgcolor={"background.default"} color={"text.primary"}>
       <Navbar />
       <Stack direction={"row"} spacing={2} justifyContent="space-between">
-      <SideBar />
+      <SideBar setMode={setMode} mode={mode}/>
       <Feed />
       <RightBar />
       </Stack>
+      <Add />
     </Box>
+    </ThemeProvider>
   );
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const SideBar = () => {
+const SideBar = ({mode,setMode}) => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
@@ -77,7 +77,7 @@ const SideBar = () => {
               <ListItemIcon>
                <DarkMode />
               </ListItemIcon>
-              <Switch ></Switch>
+              <Switch onChange={e => setMode(mode === "light"?"dark" : "light")}></Switch>
             </ListItemButton>
           </ListItem>
       </List>
